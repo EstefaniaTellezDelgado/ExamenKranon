54c0a10851677fe505c1407e7524cdfd651cb2a5
EstefaniaTellezDelgado <estefaniatellezdelgado@gmail.com>
2022-07-05 19:50:22 -0500

    Scripts

---
+++ SCRIPT SQL/Examen_Kranon_script.sql
@@ -0,0 +1,49 @@
+	BEGIN
+	INSERT INTO Libro (Titulo, AñoPublicacion, IdAutor, IdEditorial, IdGenero, NumeroPaginas, Descripcion, Imagen)
+	VALUES ('Carrie', 1974, 1, 1, 1, 254, 'Durante años, Carrie ha sido maltratada en su casa por su inestable madre, Margaret White, fanática religiosa. En el instituto las cosas no van mucho mejor: ha sido una paria desde el primer año y objeto de acoso escolar debido a sus creencias religiosas y su ropa pasada de moda.', '')
+	END
+
+	BEGIN
+	INSERT INTO Libro (Titulo, AñoPublicacion, IdAutor, IdEditorial, IdGenero, NumeroPaginas, Descripcion, Imagen)
+	VALUES ('El peregrino', 1987, 4, 4, 2, 120, 'En esta primera novela, el escritor brasileño muestra su lado más humano: sus miedos, sus demonios internos, su aprendizaje y es a través de la construcción literaria de varias parábolas que motiva al lector, no sólo a recorrer su propio camino sino a luchar por darle un sentido a su vida para realizar así su leyenda personal', '')
+	END
+
+	BEGIN
+	INSERT INTO Libro (Titulo, AñoPublicacion, IdAutor, IdEditorial, IdGenero, NumeroPaginas, Descripcion, Imagen)
+	VALUES ('Vana Respuesta', 2008, 2, 2, 2, 359, 'Vana respuesta narra la educación sentimental de Judith Earle —hija única de un padre académico y una madre perteneciente a la alta sociedad—, que vive recluida en una gran casa en el valle del Támesis.', '')
+	END
+
+	BEGIN
+	INSERT INTO Libro (Titulo, AñoPublicacion, IdAutor, IdEditorial, IdGenero, NumeroPaginas, Descripcion, Imagen)
+	VALUES ('Vana Respuesta', 2008, 2, 2, 2, 359, 'Vana respuesta narra la educación sentimental de Judith Earle —hija única de un padre académico y una madre perteneciente a la alta sociedad—, que vive recluida en una gran casa en el valle del Támesis.', '')
+	END
+
+	BEGIN
+	INSERT INTO Libro (Titulo, AñoPublicacion, IdAutor, IdEditorial, IdGenero, NumeroPaginas, Descripcion, Imagen)
+	VALUES ('El código Da Vinci', 2003, 1003, 3, 2, 499, 'El libro narra los intentos de Robert Langdon, Profesor de Iconografía Religiosa de la Universidad Harvard, para resolver el misterioso asesinato de Jacques Saunière ocurrido en el Museo del Louvre en París.', '')
+	END
+
+	BEGIN
+	INSERT INTO Libro (Titulo, AñoPublicacion, IdAutor, IdEditorial, IdGenero, NumeroPaginas, Descripcion, Imagen)
+	VALUES ('Angeles y Demonios', 2000, 1003, 1004, 2, 1, 'Con unas pocas horas para evitar el desastre, unos misteriosos ambigramas y con un asesino que siempre lleva la delantera, Langdon y una científica italiana se ponen en una carrera contrarreloj.', '')
+	END
+
+BEGIN
+	INSERT INTO Libro (Titulo, AñoPublicacion, IdAutor, IdEditorial, IdGenero, NumeroPaginas, Descripcion, Imagen)
+	VALUES ('Rabia', 1977, 1, 1, 1, 211, ' La historia comienza durante la primera hora de clase, cuando Charlie es llamado a la oficina del director a través de un intercomunicador de su clase de Álgebra II. El Director Sr. Denver quiere discutir con Charlie un incidente que ocurrió dos meses atrás, cuando Charlie casi mata a su profesor de química, al Sr. Carlson, al golpearlo en la cabeza con una llave inglesa', '')
+	END
+
+BEGIN
+	INSERT INTO Libro (Titulo, AñoPublicacion, IdAutor, IdEditorial, IdGenero, NumeroPaginas, Descripcion, Imagen)
+	VALUES ('El gato negro', 1843, 2, 2, 1, 121, 'El protagonista es un hombre dócil y bondadoso, le gustaban mucho los animales al igual que a su esposa, lleva una vida hogareña, apacible con su gato llamado Plutón, hasta que el joven empieza a dejarse arrastrar por la bebida.', '')
+	END
+
+BEGIN
+	INSERT INTO Libro (Titulo, AñoPublicacion, IdAutor, IdEditorial, IdGenero, NumeroPaginas, Descripcion, Imagen)
+	VALUES ('El cuervo', 1845, 2, 1, 1, 95, 'El texto narra la misteriosa visita de un cuervo parlante a la casa de un amante afligido, y del lento descenso hacia la locura de este último.', '')
+	END
+
+BEGIN
+	INSERT INTO Libro (Titulo, AñoPublicacion, IdAutor, IdEditorial, IdGenero, NumeroPaginas, Descripcion, Imagen)
+	VALUES ('Inferno', 2013, 1003, 3, 2, 496, 'El profesor de la Universidad de Harvard Robert Langdon se despierta en un hospital con una herida en la cabeza y no recuerda nada de los últimos días.', '')
+	END
